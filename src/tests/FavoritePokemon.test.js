59b56e0f1bd8d3f4ab9d4b06ffbcfbc49482f6f0
paulinha <paulinha@MacBook-Pro-de-seguranca.local>
2023-01-21 20:29:49 -0300

    Reenvio Requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
+// import { act } from 'react-dom/test-utils';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('', () => { });
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Page requested not found', () => {
+    renderWithRouter(<App />);
+
+    const naoEncontrado = screen.getByRole('heading', { level: 2, name: /Page requested not found/i });
+    expect(naoEncontrado).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<App />);
+
+    const imagem = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    // expect(imagem).toBeInTheDocument();
+    expect(imagem.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

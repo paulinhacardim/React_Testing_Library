bdd1df7d8e464010f3ea640f75adea0ad8c70d7c
paulinha <paulinha@MacBook-Pro-de-seguranca.local>
2023-01-23 14:55:44 -0300

    Requisito 4 com teste e stryker

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,34 +1,31 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// // import userEvent from '@testing-library/user-event';
-// import { act } from 'react-dom/test-utils';
+import { act } from 'react-dom/test-utils';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('', () => { });
 
 describe('Teste o componente <NotFound.js />', () => {
+  const pokemonNaoEncontrado = '/pokemons';
   it('Page requested not found', () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push(pokemonNaoEncontrado);
+    });
 
     const naoEncontrado = screen.getByRole('heading', { level: 2, name: /Page requested not found/i });
     expect(naoEncontrado).toBeInTheDocument();
   });
 
   it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
 
+    act(() => {
+      history.push(pokemonNaoEncontrado);
+    });
     const imagem = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
     expect(imagem).toBeInTheDocument();
     expect(imagem.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
-
-
-
-
-
-
-
-
-
